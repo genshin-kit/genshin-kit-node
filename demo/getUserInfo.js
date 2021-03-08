2916c8b198c339d229216f8966021cf7b04d946c
Dragon-Fish <824399619@qq.com>
2021-03-08 20:42:26 +0800

    Minor fix

--- demo/getUserInfo.js
+++ demo/getUserInfo.js
@@ -1,4 +1,4 @@
 const { App, uid } = require('./')
 
 // 获取用户基础信息
-App.getUserInfo(uid).then(console.log)
+App.getUserInfo(uid).then(console.log, console.error)
--- demo/getUserRoles.js
+++ demo/getUserRoles.js
@@ -1,11 +1,4 @@
 const { App, uid } = require('./')
 
 // 获取角色详细信息
-App.getUserRoles().then(
-  data => {
-    console.log(data)
-  },
-  err => {
-    console.error('error', err)
-  }
-)
+App.getUserRoles(uid).then(console.log, console.error)
