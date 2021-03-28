7ffb0774fed58115c1f8ba1b9a810a1a7c323653
Dragon-Fish <824399619@qq.com>
2021-03-28 16:20:44 +0800

    chore: minor fix (bump version: 0.4.2)

--- util/isValidCnUid.js
+++ util/isValidCnUid.js
@@ -1,7 +1,7 @@
 module.exports = (uid) => {
   uid = parseInt(uid)
   if (
-    !NaN(uid) &&
+    !isNaN(uid) &&
     String(uid).length === 9 &&
     (String(uid)[0] === '1' || String(uid)[0] === '5')
   ) {
