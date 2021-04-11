2066977b4755fe566cf4a9d2d6261493ff55fc6f
Dragon-Fish <824399619@qq.com>
2021-04-11 22:20:52 +0800

    chore: minor fix

--- demo/charactersFilter.js
+++ demo/charactersFilter.js
@@ -2,42 +2,36 @@ const { App, uid } = require('.')
 const { util } = require('..')
 
 // 使用角色过滤器
-App.getAllCharacters(uid).then(
-  data => {
-    const Filter = new util.CharactersFilter(data)
-    // 全部角色
-    console.log(`玩家 ${uid} 的全部角色`, Filter.all())
+App.getAllCharacters(uid).then((data) => {
+  const Filter = new util.CharactersFilter(data)
+  // 全部角色
+  console.log(`玩家 ${uid} 的全部角色`, Filter.all())
 
-    // 用角色名称查询详情
-    const diluke1 = Filter.name('迪卢克')
-    if (diluke1) {
-      console.log('byName', `玩家 ${uid} 的迪卢克`, diluke1)
-    } else {
-      console.log(`玩家 ${uid} 没有迪卢克`)
-    }
+  // 用角色名称查询详情
+  const diluke1 = Filter.name('迪卢克')
+  if (diluke1) {
+    console.log('byName', `玩家 ${uid} 的迪卢克`, diluke1)
+  } else {
+    console.log(`玩家 ${uid} 没有迪卢克`)
+  }
 
-    // // 也可以用角色 id 查询
-    const diluke2 = Filter.id(10000016)
-    if (diluke2) {
-      console.log('byId', `玩家 ${uid} 的 ${diluke2.name}`, diluke2)
-    } else {
-      console.log(`玩家 ${uid} 没有 id 为 10000016 的角色`)
-    }
+  // // 也可以用角色 id 查询
+  const diluke2 = Filter.id(10000016)
+  if (diluke2) {
+    console.log('byId', `玩家 ${uid} 的 ${diluke2.name}`, diluke2)
+  } else {
+    console.log(`玩家 ${uid} 没有 id 为 10000016 的角色`)
+  }
 
-    // 用星级查询
-    const byRarity = Filter.rarity(5)
-    if (byRarity) {
-      console.log(`玩家 ${uid} 的五星角色`, byRarity)
-    } else {
-      console.log(`玩家没有五星角色`)
-    }
+  // 用星级查询
+  const byRarity = Filter.rarity(5)
+  if (byRarity) {
+    console.log(`玩家 ${uid} 的五星角色`, byRarity)
+  } else {
+    console.log(`玩家没有五星角色`)
+  }
 
-    // 用元素查询
-    const pyroCharacters = Filter.element('火') // 或 pyro，中英文皆可
-    console.log(`玩家 ${uid} 的全部火系角色`, pyroCharacters)
-  },
-  err => {
-    console.error(err)
-  },
-  err => console.error
-)
+  // 用元素查询
+  const pyroCharacters = Filter.element('火') // 或 pyro，中英文皆可
+  console.log(`玩家 ${uid} 的全部火系角色`, pyroCharacters)
+}, console.error)
--- demo/getAbyss.js
+++ demo/getAbyss.js
@@ -24,7 +24,7 @@ function formater({
 Promise.all([App.getCurrentAbyss(uid), App.getPreviousAbyss(uid)]).then(
   (data) => {
     const [cur, prev] = data
-    console.log(JSON.stringify(data))
+    // console.log(JSON.stringify(data))
     console.log('当期：', formater(cur))
     console.log('往期：', formater(prev))
   },
