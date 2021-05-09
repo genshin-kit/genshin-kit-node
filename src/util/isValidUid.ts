47c7d48509a1ead7fb76c063c12741f0801391f0
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-05-09 20:42:02 +0800

    simplify & fix logical error

--- src/util/isValidUid.ts
+++ src/util/isValidUid.ts
@@ -1,38 +1,17 @@
 export function isValidCnUid(uid: any): boolean {
   const uidNum: number = parseInt(uid)
-  if (
-    isValidUid(uidNum) &&
+  return isValidUid(uidNum) &&
     ['1', '5'].includes(uidNum.toString()[0])
-  ) {
-    return true
-  }
-  else {
-    return false
-  }
 }
 
 export function isValidOsUid(uid: any): boolean {
   const uidNum: number = parseInt(uid)
-  if (
-    !isValidUid(uidNum) &&
+  return isValidUid(uidNum) &&
     ['6', '7', '8', '9'].includes(uidNum.toString()[0])
-  ) {
-    return true
-  }
-  else {
-    return false
-  }
 }
 
 function isValidUid(uid: number): boolean {
-  if (
-    !isNaN(uid) &&
+  return !isNaN(uid) &&
     uid.toString().length === 9 &&
     Math.floor(uid / 100000000) * 100000000 !== uid
-  ) {
-    return true
-  }
-  else {
-    return false
-  }
 }
