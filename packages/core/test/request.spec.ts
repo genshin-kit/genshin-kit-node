a9fa56e02ba1f736e1a808315b2c050b11d98d60
ChanIok <654232825@qq.com>
2021-09-15 21:08:32 +0800

    test: test with own uid

--- packages/core/test/request.spec.ts
+++ packages/core/test/request.spec.ts
@@ -2,16 +2,24 @@ import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import { argv } from 'process'
 import { GenshinKit } from '../src/index'
-import { playerList } from'./test-data.json'
  
 describe('Test request in request.ts', () => {
   it('should be able to get retcode 0' , async () => {
     const genshinKit = new GenshinKit()
     genshinKit.loginWithCookie(argv[4])
-    const res =  await  genshinKit.request(
+    let res =await  genshinKit.request(
+      'get',
+      'https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByCookie'
+    )
+    const user = res.data.list.find((item:any) => {
+      if(item.game_biz==='hk4e_cn' ||item.game_biz==='hk4e_global' ){
+        return true
+      }
+    })
+    res =  await  genshinKit.request(
       'get', 'index', {
-        role_id: playerList[0].role_id,
-        server:playerList[0].server,
+        role_id: user.game_uid,
+        server:user.region,
       }
     )
     expect(res.retcode).to.equal(0)
