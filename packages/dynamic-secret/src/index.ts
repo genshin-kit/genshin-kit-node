1a250191d637f86ca74d71e4cec97236ae164281
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2022-01-06 21:20:10 +0800

    update ds package

--- packages/dynamic-secret/src/index.ts
+++ packages/dynamic-secret/src/index.ts
@@ -1,25 +1,28 @@
 import crypto from 'crypto'
 import { URLSearchParams } from 'url'
 
+const CN_SALT = 'xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs'
+const OS_SALT = '6s25p5ox5y14umn1p61aqyyvbvvl3lrt'
+
 export const HOYOLAB_VERSION = {
   cn: '2.11.1',
   os: '1.5.0',
 }
 
-export function getDS(serverType: 'os' | null): string
+export function getDS(serverType?: 'os'): string
 export function getDS(
-  serverType: 'cn' | 'os' | null,
+  serverType?: 'cn' | 'os',
   {
     query,
     body,
   }: {
     query?: Record<string, string | string[]>
     body?: Record<string, unknown>
-  } | null = null
+  } = {}
 ): string {
   switch (serverType) {
     case 'os':
-      return getOsDS('6s25p5ox5y14umn1p61aqyyvbvvl3lrt')
+      return getOsDS()
     case 'cn':
     default:
       if (query && body) {
@@ -31,18 +34,18 @@ export function getDS(
 }
 
 // 生成随机字符串
-function randomString(e: number): string {
+function randomString(length: number): string {
   const s = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
   const res = []
-  for (let i = 0; i < e; ++i) {
+  for (let i = 0; i < length; ++i) {
     res.push(s[Math.floor(Math.random() * s.length)])
   }
   return res.join('')
 }
 
 // 按首字母排序 object
-function sortKeys<T extends object>(obj: T): T {
-  const copy = {} as T
+function sortKeys<T>(obj: Record<string, T>): Record<string, T> {
+  const copy: Record<string, T> = {}
   const allKeys = Object.keys(obj).sort()
   allKeys.forEach((key) => {
     copy[key] = obj[key]
@@ -50,13 +53,13 @@ function sortKeys<T extends object>(obj: T): T {
   return copy
 }
 
-export function getOsDS(salt: string): string {
+export function getOsDS(): string {
   const time = Math.floor(Date.now() / 1000)
   const random = randomString(6)
 
   const c = crypto
     .createHash('md5')
-    .update(`salt=${salt}&t=${time}&r=${random}`)
+    .update(`salt=${OS_SALT}&t=${time}&r=${random}`)
     .digest('hex')
   return `${time},${random},${c}`
 }
@@ -68,7 +71,6 @@ export function getCnDS({
   query: Record<string, string | string[]>
   body: Record<string, unknown>
 }): string {
-  const salt = 'xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs'
   const time = Math.floor(Date.now() / 1000)
   // Integer between 100000 - 200000
   const random = Math.floor(Math.random() * (200000 - 100000 + 1)) + 100000
@@ -78,7 +80,7 @@ export function getCnDS({
 
   const check = crypto
     .createHash('md5')
-    .update(`salt=${salt}&t=${time}&r=${random}&b=${b}&q=${q}`)
+    .update(`salt=${CN_SALT}&t=${time}&r=${random}&b=${b}&q=${q}`)
     .digest('hex')
 
   return `${time},${random},${check}`
