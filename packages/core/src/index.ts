8bb43cf9446a2b8ae8a0aa5c8fa94d9050d29978
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2022-01-08 22:49:35 +0800

    chore: export utils

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,7 @@
  * @desc An API wrapper for fetching player data of Genshin Impact
  *
  * @author 机智的小鱼君 <dragon-fish@qq.com>
+ * @author AlPha5130 <alpha513001@gmail.com>
  * @license Apache-2.0
  */
 
@@ -10,3 +11,4 @@
 export const name = 'genshin-kit'
 
 export { GenshinKit } from './GenshinKit'
+export * as utils from './utils'
