c0d7ed6ec596cde524831e6e1a97842acd11777f
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2022-02-11 01:06:25 +0800

    Update OsHeader.ts

--- packages/core/src/Header/OsHeader.ts
+++ packages/core/src/Header/OsHeader.ts
@@ -1,10 +1,15 @@
-import { getOsDS } from '@genshin-kit/dynamic-secret'
+import { URLSearchParams } from 'url'
+import { getOsDS, HOYOLAB_VERSION } from '@genshin-kit/dynamic-secret'
 
 export function osHeader({
   cookie,
+  query,
+  body,
   locale,
 }: {
   cookie: string
+  query?: URLSearchParams | Record<string, string | string[]>
+  body?: Record<string, any>
   locale?: string
 }): Record<string, string> {
   return {
@@ -12,12 +17,12 @@ export function osHeader({
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,en-US;q=0.8',
     Cookie: cookie,
-    DS: getOsDS(),
+    DS: getOsDS({ query, body }),
     Origin: 'https://webstatic-sea.hoyolab.com',
     Referer: 'https://webstatic-sea.hoyolab.com/',
     'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-    'x-rpc-app_version': '1.5.0',
+    'x-rpc-app_version': HOYOLAB_VERSION.os,
     'x-rpc-client_type': '5',
     'x-rpc-language': locale ?? 'zh-cn',
   }
